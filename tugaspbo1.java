305f6d6bfcac2a878e743c8e7264164a370156a3
shohabiyan11 <114603312+shohabiyan11@users.noreply.github.com>
2022-09-28 23:35:33 +0700

    Add files via upload

---
+++ tugaspbo1.java
@@ -0,0 +1,12 @@
+public class TugasNO1PBO {
+    public static void main(String[] args) {
+       System.out.println("Belajar Java");
+       System.out.println("Sangat mudah sekali");
+       System.out.println("dan sangat menyenangkan");
+   }
+}
+/*
+Nama : shohabiyan
+Kelas : TI21A
+Tugas Sesi 1 No 1 PBO
+*/
