ba5ad28acc456a14a457fdcfd42585ce962a1e03
shohabiyan11 <114603312+shohabiyan11@users.noreply.github.com>
2022-09-29 21:15:00 +0700

    Add files via upload

---
+++ TUGASNO3.java
@@ -0,0 +1,19 @@
+Heru SHOHABIYAN
+No.3 PBO Sesi 1
+Versi Terbaru Yakni Java 19
+Terdapat Beberapa kelebihan Feature Yang Baru dan juga yang lama kemudian di Modifikasi
+Antara Lain :
+1. Adanya Properti Sistem Baru untuk System.out dan System.err
+2. Adanya Metode Baru untuk Membuat HashMaps yang Sudah Dialokasikan sebelumnya
+3. Terdapat Fiture Pratinjau- dan Inkubator-Fitur
+	Fitur pratinjau dan inkubator, yaitu fitur yang belum selesai tetapi sudah dapat diuji oleh komunitas pengembang. 
+	Umpan balik dari komunitas biasanya dimasukkan ke dalam pengembangan lebih lanjut dan penyelesaian fitur-fitur ini.
+4. Adanya Virtual Threads
+	 yang merupakan utas ringan yang secara dramatis mengurangi upaya penulisan, pemeliharaan, dan pengamatan throughput tinggi, aplikasi bersamaan. 
+	 Sasarannya termasuk mengaktifkan aplikasi server yang ditulis dalam gaya thread-per-request 
+	 sederhana untuk diskalakan dengan penggunaan perangkat keras yang hampir optimal, mengaktifkan kode yang ada yang menggunakan Java.
+	 lang Thread API untuk mengadopsi thread virtual dengan perubahan minimal, 
+	 dan mengaktifkan pemecahan masalah, debugging, dan pembuatan profil utas virtual dengan alat JDK yang ada.
+5. Dengan port Linux/RISC-V
+         Java akan mendapatkan dukungan untuk set instruksi perangkat keras yang sudah didukung oleh berbagai rantai alat bahasa. RISC-V sebenarnya adalah keluarga ISA terkait
+         Footer
