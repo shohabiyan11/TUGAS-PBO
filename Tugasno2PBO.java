4bb0fe9f122f47c20801adec5918d18e56962ec2
shohabiyan11 <114603312+shohabiyan11@users.noreply.github.com>
2022-09-29 19:53:22 +0700

    Add files via upload

---
+++ Tugasno2PBO.java
@@ -0,0 +1,17 @@
+package tugasno2;
+
+import javax.swing.JOptionPane;
+public class Tugasno2PBO {
+     public static void main(String[] args) {
+        String Pesan;
+         Pesan = " ";
+         Pesan = JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        String pesan = ( "belajar " + Pesan + " sangat mudah");
+        JOptionPane.showMessageDialog(null, pesan);        
+    }
+}
+
+//*
+//Nama : shohsbiyan
+//Kelas :TI21f
+//Tugas Sesi 1 No 2
